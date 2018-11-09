e434f34caaf626070e55ba2526beb6dcec79ed62
Zach Perlman <zach.perlman@wustl.edu>
2018-11-09 11:40:36 -0600

    commit

--- 438messaging/ViewController.swift
+++ 438messaging/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    @IBOutlet var tableView: UITableView!
+    
+    var ref: DatabaseReference!
+    var databaseHandle: DatabaseHandle?
+    
+    var postData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //set firebase database reference
+        ref = Database.database().reference()
+        
+        //pull posts and listen for change
+        databaseHandle =  ref?.child("Posts").observe(.childAdded, with: {(snapshot) in
+            self.postData.append("")
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
