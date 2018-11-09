396c58a5762fd934c9cc158c497f33e088c65094
Zach Perlman <zach.perlman@wustl.edu>
2018-11-09 14:35:26 -0600

    up

--- 438messaging/ViewController.swift
+++ 438messaging/ViewController.swift
@@ -11,11 +11,13 @@ import FirebaseDatabase
 
 class ViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var textField: UITextField!
     
     var ref: DatabaseReference!
     var databaseHandle: DatabaseHandle?
     
     var postData = [String]()
+    var time:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +30,33 @@ class ViewController: UIViewController {
         databaseHandle =  ref?.child("groups").child("testGroup").observe(.childAdded, with: {(snapshot) in
             self.postData.append("")
         })
+        tableView.dataSource = postData as? UITableViewDataSource
+        updateMessages()
     }
-
+    @IBAction func sendButton(_ sender: Any) {
+        
+//        time = time+1
+//        self.ref.child("groups/\testGroup/").child("testGroup").setValue([String(time): "hi"])
+        
+        
+        let key = ref?.child("posts").childByAutoId().key
+        let userID = 4
+        let username = "user"
+        let title = "title"
+        let body = textField.text
+        let post = ["uid": userID,
+                    "author": username,
+                    "title": title,
+                    "body": body] as [String : Any]
+        let childUpdates = ["/posts/\(String(describing: key))": post,
+                            "/user-posts/\(userID)/\(String(describing: key))/": post]
+        ref.updateChildValues(childUpdates)
+    }
+    
+    func updateMessages() {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
